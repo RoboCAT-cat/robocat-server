c824364c4a73708f414d11db950e0701ffeaf6ec
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-03-02 13:07:54 +0100

    Add first version of Match API

--- matches/schema.py
+++ matches/schema.py
@@ -9,6 +9,10 @@ class MatchType(DjangoObjectType):
 
 class Query:
     all_matches = graphene.List(graphene.NonNull(MatchType))
+    match = graphene.Field(MatchType, matchId=graphene.UUID(required=True))
 
     def resolve_all_matches(self, info, **kwargs):
         return Match.objects.all()
+
+    def resolve_match(self, info, matchId, **kwargs):
+        return Match.objects.filter(id=matchId).first()
